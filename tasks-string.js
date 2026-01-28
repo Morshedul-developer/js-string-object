9e94eced25e7c3d73186d56baa0d5d9005697c41
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-01-28 17:06:20 +0600

    practiced some important tasks about js string

---
+++ tasks-string.js
@@ -0,0 +1,54 @@
+//* Task-1:
+// Count how many times a string has the letter a or A.
+
+// solution
+
+// const str = "Hello I am learning javascript aAa.";
+// let sum = 0;
+
+// for(let i=0; i<str.length; i++) {
+//     if(str[i].toLowerCase() === 'a'){
+//         sum+=1;
+//     }
+// }
+// console.log("Total number of a or A is :",sum);
+
+
+//* Task-3:
+// Check whether a string contains all the vowels a, e, i, o, u
+
+// solution
+// const str = "Hello I am very excited to know from you.";
+// let isA = false;
+// let isE = false;
+// let isI = false;
+// let isO = false;
+// let isU = false;
+
+
+// for(let i=0; i<str.length; i++) {
+//     if(str[i].toLowerCase() === "a"){
+//         isA = true;
+//     }
+//     else if(str[i].toLowerCase() === "e"){
+//         isE = true;
+//     }
+//     else if(str[i].toLowerCase() === "i"){
+//         isI = true;
+//     }
+//     else if(str[i].toLowerCase() === "o"){
+//         isO = true;
+//     }
+//     else if(str[i].toLowerCase() === "u"){
+//         isU = true;
+//     }
+// }
+
+// if(isA && isE && isI && isU && isU) {
+//     console.log("Yes, all vowels are present in this sentence.");
+// }
+// else{
+//     console.log("No, all vowels are not present here.");
+// }
+
+
