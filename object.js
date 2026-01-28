d246f11edca47d8798588614ef5694d18b57b1e2
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-01-28 10:56:35 +0600

    explored the object declaration in js

---
+++ object.js
@@ -0,0 +1,11 @@
+// Object declaration
+
+const myObject = {
+  name: "Sample Object",
+  type: "Demo",
+  arr: [1, 2, 3, 4, 5],
+    details: {
+    created: "2024-01-01",
+    updated: "2024-06-01"
+  }
+};
