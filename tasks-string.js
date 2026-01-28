dac49c3ff719d7574cf38e14674f2795a4c79ede
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-01-28 19:20:36 +0600

    solved another problem for js string and also explored a new array method map()

--- tasks-string.js
+++ tasks-string.js
@@ -17,6 +17,7 @@
 // Check whether a string contains all the vowels a, e, i, o, u
 
 // solution
+
 // const str = "Hello I am very excited to know from you.";
 // let isA = false;
 // let isE = false;
@@ -49,10 +50,12 @@
 //     console.log("No, all vowels are not present here.");
 // }
 
-// Task-3:
+//* Task-3:
 // If a given string has either x, replace x by y. if the given string has X, replace it by Y.
 
-const str = "Hello xx to XX";
+// solution
+
+// const str = "Hello xx to XX";
 // const str = "Hello";
 
 // if (str.includes("x")) {
@@ -62,10 +65,32 @@ const str = "Hello xx to XX";
 // else{
 //     console.log("This string has no x or X.");
 // }
-if (str.includes("x")) {
-  const newStr = str.split('x').join('y').split('X').join('Y');
-  console.log(newStr);
-}
-else{
-    console.log("This string has no x or X.");
-}
+// if (str.includes("x")) {
+//   const newStr = str.split('x').join('y').split('X').join('Y');
+//   console.log(newStr);
+// }
+// else{
+//     console.log("This string has no x or X.");
+// }
+
+
+//* Task-4:
+// Capitalize Every first Letter of each word in a String
+
+// solution
+
+const str = "Hello I am very excited to know from you.";
+// const words = str.split(' ');
+// let newStr = "";
+
+// for(const word of words) {
+//     const newWord = word[0].toUpperCase() + word.slice(1);
+//     newStr = newStr + " " + newWord;
+// }
+// console.log(newStr.trim());
+
+
+//! With map()
+const newStr = str.split(' ').map(word => word[0].toUpperCase() + word.slice(1)).join(' ');
+
+console.log(newStr);
